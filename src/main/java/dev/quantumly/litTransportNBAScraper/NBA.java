0f0e195a3f587c3561b31f4c9c95a3a62e1e61d6
QuantumlyTangled <ethoarx@gmail.com>
2021-04-27 14:53:48 +0200

    chore: add editor config

--- src/main/java/dev/quantumly/litTransportNBAScraper/NBA.java
+++ src/main/java/dev/quantumly/litTransportNBAScraper/NBA.java
@@ -14,7 +14,8 @@ import picocli.CommandLine.Parameters;
 @Command(name = "nba", mixinStandardHelpOptions = true, version = "1.0.0")
 public class NBA implements Callable<Integer> {
 
-  @Parameters(paramLabel = "PLAYER", arity = "1...") String[] player;
+  @Parameters(paramLabel = "PLAYER", arity = "1...")
+  String[] player;
 
   public static void main(String[] args) {
     System.exit(new CommandLine(new NBA()).execute(args));
@@ -39,6 +40,7 @@ public class NBA implements Callable<Integer> {
 
   /**
    * Return the passed player
+   *
    * @return The player which was passed to the command as the main parameter
    */
   public String[] getPlayer() {
@@ -47,30 +49,38 @@ public class NBA implements Callable<Integer> {
 
   /**
    * Determines a profile page URL from a joined name
+   *
    * @param profile The search string from which to determine a profile URL
-   * @return The final URL to use for scraping a profile page after a potential redirect has been executed
+   * @return The final URL to use for scraping a profile page after a potential redirect has been
+   * executed
    */
   private static String getRedirectableSearch(String profile) throws IOException {
-    Connection initialPlayerProfile = Jsoup.connect("https://www.basketball-reference.com/search/search.fcgi?search=" + profile);
+    Connection initialPlayerProfile = Jsoup
+      .connect("https://www.basketball-reference.com/search/search.fcgi?search=" + profile);
     Response followedUrl = initialPlayerProfile.followRedirects(true).execute();
 
     // It's better to try and assume that we will be redirected since a potential call to the website is rather costly.
     // Both in time and in the risk of a potential IP blacklist.
     if (followedUrl.url().toString().contains("?")) {
-      Document searchDocument = Jsoup.connect("https://www.basketball-reference.com/search/search.fcgi?search=" + profile).get();
-      return "https://www.basketball-reference.com" + searchDocument.select(".search-item-name").first().select("a").first().attr("href");
-    } else return followedUrl.url().toString();
+      Document searchDocument = Jsoup
+        .connect("https://www.basketball-reference.com/search/search.fcgi?search=" + profile).get();
+      return "https://www.basketball-reference.com" + searchDocument.select(".search-item-name")
+        .first().select("a").first().attr("href");
+    } else
+      return followedUrl.url().toString();
   }
 
   /**
    * Extracts the centre values from a specified "data-stat"
+   *
    * @param document The `Document` on which the selection should be performed
-   * @param stat The "data-stat" property that should be checked
+   * @param stat     The "data-stat" property that should be checked
    * @return An array of entries with the total count and header removed
    */
   private static Object[] extractTableValues(Document document, String stat) {
     var Raw = document.select("[data-stat=" + stat + "]").eachText().toArray();
-    var RawFiltered = Arrays.stream(Raw).filter(e -> !(e.toString().contains("season") || e.toString().contains("Career"))).toArray();
+    var RawFiltered = Arrays.stream(Raw)
+      .filter(e -> !(e.toString().contains("season") || e.toString().contains("Career"))).toArray();
     return Arrays.copyOfRange(RawFiltered, 1, RawFiltered.length);
   }
 }
