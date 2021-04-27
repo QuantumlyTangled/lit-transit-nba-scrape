a1802536bc67e5b34bc2dc9cce392ae6166a5cf1
QuantumlyTangled <ethoarx@gmail.com>
2021-04-27 07:40:53 +0200

    feat(test): try running command

--- src/test/java/dev/quantumly/litTransportNBAScraper/NBATest.java
+++ src/test/java/dev/quantumly/litTransportNBAScraper/NBATest.java
@@ -1,10 +1,16 @@
 package dev.quantumly.litTransportNBAScraper;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import picocli.CommandLine;
 
 public class NBATest {
+
+  @Rule
+  public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
   @Test
   public void testBasicNBAParametersPassing() {
@@ -13,7 +19,21 @@ public class NBATest {
     new CommandLine(nba).execute(args);
 
     String[] player = nba.getPlayer();
-    Assertions.assertArrayEquals(player, args);
+    assertArrayEquals(args, player);
+  }
+
+  @Test
+  public void testNBARun() {
+    String[] args = new String[] { "Luka", "Doncic" };
+    NBA nba = new NBA();
+    new CommandLine(nba).execute(args);
+
+    assertEquals(String.join(System.lineSeparator(), new String[] {
+        "2018-19 7.1",
+        "2019-20 8.9",
+        "2020-21 8.3",
+        ""
+    }), systemOutRule.getLog());
   }
 
 }
